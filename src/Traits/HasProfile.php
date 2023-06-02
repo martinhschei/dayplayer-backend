7154ba09055ac3cccee243175833829e0fe711af
Martin Schei <martinhschei@gmail.com>
2023-06-03 00:05:24 +0200

    removed dep

--- src/Traits/HasProfile.php
+++ src/Traits/HasProfile.php
@@ -5,9 +5,7 @@ namespace Dayplayer\BackendModels\Traits;
 use Dayplayer\BackendModels\Profile;
 
 trait HasProfile
-{
-    use HybridRelations;
-    
+{    
     public function profileId()
     {
         return $this->profile->id;
