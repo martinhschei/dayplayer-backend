2128035b29a480d8ac0a59ecd126f4d803c6c73a
Martin Schei <martinhschei@gmail.com>
2023-06-03 00:13:19 +0200

    added name to users table

--- src/migrations/2013_11_22_191028_create_users_table.php
+++ src/migrations/2013_11_22_191028_create_users_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('type');
+            $table->string('name');
             $table->string('password');
             $table->string('email')->unique();
             $table->string('profile_id')->nullable();
