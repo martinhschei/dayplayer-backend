616a3fb2adbeaa0af25024f7183120ce99d4ffb3
Martin Schei <martinhschei@gmail.com>
2023-07-02 22:45:23 +0200

    added production requests

--- src/DayplayerBackendModelsServiceProvider.php
+++ src/DayplayerBackendModelsServiceProvider.php
@@ -10,7 +10,7 @@ class DayplayerBackendModelsServiceProvider extends ServiceProvider
     {
         
     }
-
+    
     public function boot(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/migrations/');
---
+++ src/requests/StoreProductionRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductionRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'end_date' => ['required', 'date'],
+            'start_date' => ['required', 'date'],
+            'departments'=> ['required', 'min:1'],
+            'payment_method' => ['required', 'string'],
+            'production_type' => ['required', 'string'],
+            'name' => ['required', 'string',  'max:150'],
+            'departments.*.name' => ['required', 'string'],
+            'departments.*.manager_name' => ['required', 'string'],
+            'departments.*.manager_email' => ['required', 'email'],
+        ];
+    }
+}
