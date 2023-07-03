1d538ea0f9c999ce9dedd508fb67f28e60f58e0c
Martin Schei <martinhschei@gmail.com>
2023-07-03 20:58:39 +0200

    auto

---
+++ src/Helpers/PaymentsManager.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Arr;
+use Dayplayer\BackendModels\Payment;
+use Dayplayer\BackendModels\Helpers\PriceCalculator;
+
+class PaymentsManager
+{
+    public static function createPaymentsForProduction(Production $production)
+    {
+        $price = new PriceCalculator(
+            $production->start_date,
+            $production->end_date,
+            $production->departments()->count()
+        );
+        
+        $priceOption = $price->getOptionsForPaymentMethod($production->payment_method);
+        
+        collect($priceOption['payment_dates'])->each(function ($paymentDate) use ($priceOption) {
+            $production->payments()->create([
+                'due_date' => $paymentDate,
+                'method' => $prodution->payment_method,
+                'status' => Payment::PAYMENT_STATUS_PENDING,
+                'amount' => Arr::get($priceOption, 'price'),
+            ]);
+        });
+    }
+}
