e6d44336f6301e825bfb2ce99245679f307ff15c
Martin Schei <martinhschei@gmail.com>
2023-07-03 18:00:19 +0200

    added form request for update production

--- src/FormRequests/UpdateProductionRequest.php
+++ src/FormRequests/UpdateProductionRequest.php
@@ -4,20 +4,19 @@ namespace Dayplayer\BackendModels\FormRequests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreProductionRequest extends FormRequest
+class UpdateProductionRequest extends FormRequest
 {
     public function authorize(): bool
     {
         return true;
     }
-    
+
     public function rules(): array
     {
         return [
             'end_date' => ['required', 'date'],
             'start_date' => ['required', 'date'],
             'departments'=> ['required', 'min:1'],
-            'payment_method' => ['required', 'string'],
             'production_type' => ['required', 'string'],
             'name' => ['required', 'string',  'max:150'],
             'departments.*.name' => ['required', 'string'],
