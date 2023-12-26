de41031e1d3f58ed94bb3ae813fee5b6cd471385
Martin Schei <martinhschei@gmail.com>
2023-12-26 21:33:17 +0100

    account setup token

--- src/AccountSetupToken.php
+++ src/AccountSetupToken.php
@@ -9,10 +9,15 @@ use Dayplayer\BackendModels\BaseModel;
 class AccountSetupToken extends BaseModel
 {
     public static function createForUser(User $user)
-    {        
+    {      
         return AccountSetupToken::create([
             'user_id' => $user->id,
             'value' => Str::uuid()->toString(),
         ]);
+    }
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
