b554c3b2c18987761828766981e03a1da8ca798f
Martin Schei <martinhschei@gmail.com>
2024-01-06 21:31:40 +0100

    switched to productions

--- src/User.php
+++ src/User.php
@@ -77,7 +77,7 @@ class User extends Authenticatable
             return null;
         }
         
-        return $this->departmentLevelProductions()->whereHas('departments', function ($query) {
+        return $this->productions()->whereHas('departments', function ($query) {
             $query->where('id', $this->current_department_id);
         })->first()->departments->first();
     }
